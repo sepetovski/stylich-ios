22a767664e0a813ca2783ec8b1c931fb2ab48438
damjan <sepetovskidamjan@gmail.com>
2026-06-03 01:48:30 +0200

    Initial Commit

---
+++ styleapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  styleapp
+//
+//  Created by NCAdevice2 on 03/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ styleapp/styleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  styleappApp.swift
+//  styleapp
+//
+//  Created by NCAdevice2 on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct styleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
