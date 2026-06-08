b3fe4f87eb3e7e486c1193c4111f6882828aa91e
sepetovski <sepetovskidamjan@gmail.com>
2026-06-08 21:33:02 +0200

    remove Config.swift from tracking

--- styleapp/Services/Config.swift
+++
@@ -1,4 +0,0 @@
-enum Config {
-    static let supabaseURL = "https://yxlpxyhqeoidoguorbev.supabase.co"
-    static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4bHB4eWhxZW9pZG9ndW9yYmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0OTQzMTAsImV4cCI6MjA5NjA3MDMxMH0.G8J7vI3U4xalOsE_L6GzvqI2Ms_nvBdON5E-kOj2MnA"
-}
